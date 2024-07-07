70fcf842e772cbe9de39d07c90aa3cabcb5a9a28
Andy10s <andrew.jarrett10s@Gmail.com>
2024-07-07 15:17:17 -0400

    fixed bug with txt reading properly

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def main():
     book_path = "books/frankenstein.txt"
+    print(f"Trying to read from: {book_path}")
     text = get_book_text(book_path)
+    print(f"Text length: {len(text)} characters")
     print(text)
-    
-def get_book_text(path):  
+
+def get_book_text(path):
     with open(path) as f:
+        print("File opened successfully")
         return f.read()
-        
-        
+
 main()
